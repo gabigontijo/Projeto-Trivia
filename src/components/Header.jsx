7c7d598b4bd79f071ef5ac7d0bd1bc4ea8967836
wanderson-rpf <wandersonrpf@gmail.com>
2022-11-18 11:58:10 -0300

    :recycle: [refactor] Movendo botão settings para o header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,33 @@ import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
 
 class Header extends React.Component {
+  handleSettings = () => {
+    const { history } = this.props;
+    history.push('/settings');
+  };
+
   render() {
     const { name, score, email } = this.props;
     // console.log(this.props);
     const hash = md5(email).toString();
     return (
       <header>
-        <img data-testid="header-profile-picture" src={ `https://www.gravatar.com/avatar/${hash}` } alt="imagem de perfil" />
+        <img
+          data-testid="header-profile-picture"
+          src={ `https://www.gravatar.com/avatar/${hash}` }
+          alt="imagem de perfil"
+        />
         <p data-testid="header-player-name">{`Nome: ${name}`}</p>
-        <p data-testid="header-score" value={ score }>{ score }</p>
+        <p data-testid="header-score" value={ score }>
+          {score}
+        </p>
+        <button
+          data-testid="btn-settings"
+          type="button"
+          onClick={ this.handleSettings }
+        >
+          Settings
+        </button>
       </header>
     );
   }
@@ -27,6 +45,9 @@ Header.propTypes = {
   email: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
