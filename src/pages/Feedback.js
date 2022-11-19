2c4369a11816318cab1f0d2e23c930ca413be1a5
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-11-19 22:24:34 +0000

    finalizando  Css pg feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { MD5 } from 'crypto-js';
 import logoTrivia from '../img/logoTrivia.png';
+import '../style/Feedback.css';
 
 class Feedback extends React.Component {
   handleClickPlayAgain = () => {
@@ -20,57 +21,68 @@ class Feedback extends React.Component {
     const acertos = 3;
     const hash = MD5(email).toString();
     return (
-      <div>
-        <div>
-          <img className="logo" src={ logoTrivia } alt="logo Trivia" />
-        </div>
-        <div className="feedback__containerAvatar">
-          <img data-testid="header-profile-picture" src={ `https://www.gravatar.com/avatar/${hash}` } alt="imagem de perfil" />
-        </div>
-        <div>
-          {
-            assertions < acertos
-              ? (
-                <h1
-                  data-testid="feedback-text"
-                  className="feekback__erros"
-                >
-                  Podia ser melhor...
-
-                </h1>
-              )
-
-              : (
-                <h1
-                  data-testid="feedback-text"
-                  className="feekback__acertos"
-                >
-                  Mandou Bem!
-
-                </h1>
-              )
-          }
-          <p data-testid="feedback-total-question">
-            {` Você acertou ${assertions} questões!` }
-
-          </p>
-          <p data-testid="feedback-total-score">
-            {` Um total de ${score} pontos`}
-          </p>
-          <div>
-            <button
-              data-testid="btn-play-again"
-              type="button"
-              onClick={ this.handleClickPlayAgain }
+      <div className="feedback">
+        <div className="feedback__container">
+          <div className="feedback__logo">
+            <img className="logo" src={ logoTrivia } alt="logo Trivia" />
+          </div>
+          <div className="feedback__pontos">
+            <div
+              className="feedback__containerAvatar"
             >
-              Play Again
-            </button>
+              <img
+                data-testid="header-profile-picture"
+                src={ `https://www.gravatar.com/avatar/${hash}` }
+                alt="imagem de perfil"
+                className={ assertions < acertos
+                  ? 'feedback__containerAvatar_error'
+                  : 'feedback__containerAvatar_acerto' }
+              />
+            </div>
+            {
+              assertions < acertos
+                ? (
+                  <h1
+                    data-testid="feedback-text"
+                    className="feekback__erros"
+                  >
+                    Podia ser melhor...
+
+                  </h1>
+                )
+
+                : (
+                  <h1
+                    data-testid="feedback-text"
+                    className="feekback__acertos"
+                  >
+                    Mandou Bem!
+
+                  </h1>
+                )
+            }
+            <p data-testid="feedback-total-question">
+              {` Você acertou ${assertions} questões!` }
+
+            </p>
+            <p data-testid="feedback-total-score">
+              {` Um total de ${score} pontos`}
+            </p>
+          </div>
+          <div className="feedback__btns">
             <button
               data-testid="btn-ranking"
               type="button"
               onClick={ this.handleClickRancking }
             >
-              Ranking
+              Ver Ranking
+            </button>
+            <button
+              data-testid="btn-play-again"
+              type="button"
+              onClick={ this.handleClickPlayAgain }
+            >
+              Jogar Novamente
             </button>
           </div>
         </div>
