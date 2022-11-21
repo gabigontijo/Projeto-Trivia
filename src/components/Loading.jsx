0ac6b69a86940ec7a951facfdb476743d6a2a1d0
wanderson-rpf <wandersonrpf@gmail.com>
2022-11-21 13:26:23 -0300

    :tada: [init] Iniciando componente loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import { MdOutlineTimer } from 'react-icons/md';
+import { timerDesablesButton } from '../redux/action';
+import style from '../style/Game.module.css';
+
+class Loading extends React.Component {};
+
+export default Loading;
